0cee7ca00dab8c9543afa4bd1adea71003a1c660
bothboss <bothboss21@gmail.com>
2015-03-31 23:48:41 +0900

    HTTP通信関数の正常起動を確認。これによる戦車の操作は未確認。

--- player/player/mytank.cpp
+++ player/player/mytank.cpp
@@ -26,6 +26,7 @@ CMytank::CMytank(){
 void CMytank::move(tstring direction){
 	//2015/3/31時点では正常運転のみ実装
 	//通信失敗の時の処理やプレイヤー状態変更の場合は考慮しない。
+	//とりあえず、プロジェクトに関係ない別のURLでは正常に通信を終了した。
 	tstring strUrl = _T("http://") + ipAddress+_T("/move/");
 	bool isMethodGet = true;
 	tstring strResult;

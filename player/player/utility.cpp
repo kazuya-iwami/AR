10bec2de8dad2ce1a6c545c0f493a8491b68aa2c
MasudaTakashi <takashi.masuda.1202.gae@gmail.com>
2015-04-11 05:18:42 +0900

    画像の大きさ調整

--- player/player/utility.cpp
+++ player/player/utility.cpp
@@ -7,11 +7,12 @@
 bool CSystem_timer::draw(){
 	//残り時間表示
 	if(system_timer > 0){
-		DrawGraph(LEFT_WINDOW_WIDTH+500-87, 20, figure_id["F_TIMER_BASE2"], true); //背景出力
+		DrawGraph(LEFT_WINDOW_WIDTH+500-65, 20, figure_id["F_TIMER_BASE2"], true); //背景出力
+
 		std::ostringstream sout;
 		sout << std::setfill('0') << std::setw(2) << (system_timer/30 + 1)%60;
 		std::string s = sout.str();
-		DrawOriginalString(440+LEFT_WINDOW_WIDTH, 35, 1.0, 24, to_string((system_timer/30 + 1)/60)+":"+s); //文字出力
+		DrawOriginalString(440+LEFT_WINDOW_WIDTH, 25, 1.0, 24, to_string((system_timer/30 + 1)/60)+":"+s); //文字出力
 		system_timer--;
 	}else finish_flag = true;
 

41e9fa665a5839783f6ce38d6addc0fb64b6b38b
kazuya-iwami <k.bluewing.i@gmail.com>
2015-04-11 07:43:22 +0900

    時間変更

--- player/player/main.cpp
+++ player/player/main.cpp
@@ -22,7 +22,7 @@
 using namespace std;
 
 #define FOCUS_SPEED 8
-#define GAME_TIME 600 //プレー時間　20秒
+#define GAME_TIME 30 //プレー時間　20秒
 #define FINISH_TIME 5 //結果発表の時間 5秒
 
 #define USE_CAMERA_FLAG 1   //0:画像 1:カメラ 2:ラズパイ

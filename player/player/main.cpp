3da2751db3feb55dcf8d7fb37f9a0cbb00aa25f2
MasudaTakashi <takashi.masuda.1202.gae@gmail.com>
2015-04-13 23:02:36 +0900

    矢印キー操作あり

--- player/player/main.cpp
+++ player/player/main.cpp
@@ -27,7 +27,7 @@ using namespace std;
 
 #define USE_CAMERA_FLAG 0   //0:画像 1:カメラ 2:ラズパイ
 
-#define PLAYER_NM 0	//自分のプレイヤー番号
+#define PLAYER_NM 0	//自分のプレイヤー番号 0～3
 #define IP_ADDRESS "172.16.100.41"	//IPアドレス
 
 
@@ -235,7 +235,7 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine
 					speed = _T("half");
 				}
 
-				/*
+				
 				//各キーを押し続けるとその動作をする。
 				if(  key_buf[ KEY_INPUT_UP ] == 1 && key_prev_buf[ KEY_INPUT_UP] == 0 ){
 				//mytank->set_vel(1,1);//自機の速度設定 (pwm制御の場合か)
@@ -260,7 +260,7 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine
 				(key_buf[ KEY_INPUT_RIGHT ] == 0 && key_prev_buf[ KEY_INPUT_RIGHT] == 1) ){
 				mytank->move(_T("stop"), speed);
 				}
-				*/
+				
 				//bullet生成
 				if(  key_buf[ KEY_INPUT_SPACE ] == 1 && key_prev_buf[ KEY_INPUT_SPACE] == 0){
 					mytank->gen_bullet(BULLET_KIND::BULLET_NOMAL);

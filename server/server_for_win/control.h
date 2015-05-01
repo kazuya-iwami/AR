33fe9d21524119da77a37efdcbe5809d7862e1f5
kazuya-iwami <k.bluewing.i@gmail.com>
2015-05-02 04:27:13 +0900

    サーバーをwinに移植

---
+++ server/server_for_win/control.h
@@ -0,0 +1,22 @@
+﻿//
+// Created by Iwami kazuya on 2015/03/22.
+//
+
+#ifndef _AR_SOCKET_MAIN_H_
+#define _AR_SOCKET_MAIN_H_
+
+#include <time.h>
+#include "server.h"
+
+extern GAME_STATUS game_status;//ゲームステータス
+extern int left_time;//今のところ実装放置
+extern CPlayer_param player_param[4];
+extern time_t item_start_time[4], item_end_time;
+extern time_t dead_start_time[4], dead_end_time;
+
+using namespace std;
+
+
+void recv_message(string msg, int id);
+
+#endif //_AR_SOCKET_MAIN_H_
---
+++ server/server_for_win/server.h
@@ -0,0 +1,66 @@
+﻿//
+// Created by Iwami kazuya on 2015/03/22.
+//
+
+#ifndef _AR_SOCKET_SERVER_H_
+#define _AR_SOCKET_SERVER_H_
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include <WinSock.h>
+
+
+enum BULLET_KIND{
+	BULLET_NOMAL,
+	BULLET_3D
+};
+
+enum ITEM_KIND{
+	ITEM_NONE,
+	STAR,
+	THUNDER
+};
+
+enum VIABILITY_STATUS{//生存状況
+	ALIVE,
+	DEAD
+};
+
+enum GAME_STATUS {
+	GAME_UNCONNECTED,
+	GAME_WAIT,
+	GAME_PLAY,
+	GAME_PAUSE,
+	GAME_FINISH
+};
+
+enum COMMAND_NAME{
+	FINISH,
+	USE_ITEM,
+	INFORM_ITEM,
+	SHOOT_BULLET,
+	RETURN_BULLET,
+	CHANGE_STATUS,
+	UPDATE_LOCATIONS,
+	DISCONNECT,
+	INFORM_DIE,
+	INFORM_REVIVE
+};
+
+class CPlayer_param{
+public:
+	CPlayer_param();
+	void init();
+
+	int score;//スコア
+	ITEM_KIND using_item;//使用していなかったら-1
+	bool exist;//接続が切れたらfalse
+	bool finish_flag;
+	VIABILITY_STATUS viability;//生存状況を保持しておく
+};
+
+void send_message(std::string msg, int id);
+
+std::string encode(COMMAND_NAME command_name, int player_from, int player_to, int kind);
+
+#endif //_AR_SOCKET_SERVER_H_

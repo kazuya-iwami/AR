b1e32072d2325eae4c6888ead805ae51af1f9107
pazudaruma <pazududaruma@gmail.com>
2015-05-15 19:58:08 +0900

    2回目落ちる修正

--- player/player/main.cpp
+++ player/player/main.cpp
@@ -434,6 +434,7 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine
 				auto wait_ = make_shared<CWait>();
 				wait = wait_;
 				CObject::register_object(wait,DRAW_LAYER::IMFOMATION_LAYER);
+				mytank->get_mode(&(wait->mode));
 				
 			}
 
